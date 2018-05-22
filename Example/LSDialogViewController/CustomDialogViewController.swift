1785f5615e23e9f5c2b6fd2da210a3ff4591e09e
Daisuke Hasegawa <daisuke_hasegawa@librastudio.co.jp>
2018-05-23 00:58:22 +0900

    Fix height and width of CustomDialogViewController

--- Example/LSDialogViewController/CustomDialogViewController.swift
+++ Example/LSDialogViewController/CustomDialogViewController.swift
@@ -14,6 +14,10 @@ class CustomDialogViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+            
+        // adjust height and width of dialog
+        self.view.bounds.size.height = UIScreen.main.bounds.size.height * 0.6
+        self.view.bounds.size.width = UIScreen.main.bounds.size.width * 0.8
     }
 
     // close dialogView
